8e3c7650a57f32ee688ba2ce37e331218c2a245c
ITS-DNY <dnyandeepgaonkar23@gmail.com>
2024-06-16 13:33:49 +0530

    second Commit

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "pages/Home";
 import NotFound from "pages/NotFound";
+import { ParallaxProvider } from "react-scroll-parallax";
 const Rdparty = React.lazy(() => import("pages/Rdparty"));
 const ProjectRoutes = () => {
   return (
+    <ParallaxProvider>
     <React.Suspense fallback={<>Loading...</>}>
       <Router>
         <Routes>
@@ -13,7 +15,7 @@ const ProjectRoutes = () => {
           <Route path="/" element={<Rdparty />} />
         </Routes>
       </Router>
-    </React.Suspense>
+    </React.Suspense></ParallaxProvider>
   ); 
 };
 export default ProjectRoutes;
